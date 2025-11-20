41bc55b72e97c1c96289e678af22ff4b7c95db26
Sir Kirby <sir.kirby49@gmail.com>
2025-11-20 10:54:33 -0500

    add stat trackers

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 function HelloWRLD() {
 document.getElementById("output1").innerHTML = "Hello World!";
-console.log("Hello World!");
+console.log("HELLO YOU!");
 }
   
 let text
@@ -10,4 +10,15 @@ text = "Hello there!";
 text = "Good Day to You!";
 }
 document.getElementById("output2").innerHTML = text; }
-
+let stat1 = document.getElementById("FirstINT").innerHTML.valueAsNumber;
+function firstChange() { 
+document.getElementById("FirstResult").innerHTML = stat1;
+}
+function oneadd1() {
+console.log(stat1);
+stat1 += 1;
+console.log("+1");
+console.log(stat1);
+document.getElementById("FirstResult").innerHTML.valueAsNumber = stat1;
+console.log(document.getElementById("FirstResult").innerHTML.valueAsNumber);
+}
