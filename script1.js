619828717770c9c216782c117d8ca28313454015
Sir Kirby <sir.kirby49@gmail.com>
2025-11-21 10:16:49 -0500

    fixed stat tracker errors
    YIPPEE!!

--- script1.js
+++ script1.js
@@ -11,13 +11,9 @@ text = "Good Day to You!";
 }
 document.getElementById("output2").innerHTML = text; }
 
-//var stat1 = 1
-Number(document.getElementById("FirstINT").innerHTML)
-/* let stat1.globalThis = 0*/
-// stat1 += FirstINT.value 
 stat1 = FirstINT.value 
 function firstChange() { let stat1 = FirstINT.value;
-//document.getElementById("FirstINT").innerHTML.value;
+
 document.getElementById("FirstResult").innerHTML = stat1;
 
 }
@@ -43,5 +39,67 @@ document.getElementById("FirstResult").innerHTML = stat1;
 FirstINT.value = stat1;
 console.log(FirstINT.value);
 console.log(stat1);
+
+}
+
+stat2 = SecondINT.value 
+function SecondChange() { let stat2 = SecondINT.value;
+
+document.getElementById("SecondResult").innerHTML = stat2;
+
+}
+
+function oneadd2() {  let stat2 = Number(SecondINT.value);
+console.log(stat2);
+stat2 = stat2 + 1;
+console.log("+1");
+console.log(stat2);
+document.getElementById("SecondResult").innerHTML = stat2;
+SecondINT.value = stat2;
+console.log(SecondINT.value);
+console.log(stat2);
+
+}
+
+function oneminus2() { let stat2 = Number(SecondINT.value);
+console.log(stat2);
+stat2 = stat2 - 1;
+console.log("-1");
+console.log(stat2);
+document.getElementById("SecondResult").innerHTML = stat2;
+SecondINT.value = stat2;
+console.log(SecondINT.value);
+console.log(stat2);
+
+}
+
+stat3 = ThirdINT.value 
+function ThirdChange() { let stat3 = ThirdINT.value;
+
+document.getElementById("ThirdResult").innerHTML = stat3;
+
+}
+
+function oneadd3() {  let stat3 = Number(ThirdINT.value);
+console.log(stat3);
+stat3 = stat3 + 1;
+console.log("+1");
+console.log(stat3);
+document.getElementById("ThirdResult").innerHTML = stat3;
+ThirdINT.value = stat3;
+console.log(ThirdINT.value);
+console.log(stat3);
+
+}
+
+function oneminus3() { let stat3 = Number(ThirdINT.value);
+console.log(stat3);
+stat3 = stat3 - 1;
+console.log("-1");
+console.log(stat3);
+document.getElementById("ThirdResult").innerHTML = stat3;
+ThirdINT.value = stat3;
+console.log(ThirdINT.value);
+console.log(stat3);
 
 }
